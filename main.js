db2e2ab7a7c001dd1c354386ab104cdcf00e270f
Marius STEPHANY <marius.stephany@gmail.com>
2023-10-11 17:04:29 +0200

    :sparkles: Update colors with GUI

--- main.js
+++ main.js
@@ -11,7 +11,11 @@ import Render from "./experience/Render";
 // Constants
 const dices = [];
 const settings = {
+  color: 0x0000ff,
   font: 1,
+  metalness: 0,
+  roughness: 0,
+  textColor: 0xffffff,
   totalDices: 2,
   throw: throwDices,
 };
@@ -37,18 +41,17 @@ const fontLoader = new FontLoader();
 
 // Materials
 const material = new THREE.MeshStandardMaterial({
-  color: 0x0000ff,
-  metalness: 0,
-  roughness: 0,
+  color: settings.color,
+  metalness: settings.metalness,
+  roughness: settings.roughness,
 });
-const textMaterial = new THREE.MeshBasicMaterial({ color: "white" });
+const textMaterial = new THREE.MeshBasicMaterial({ color: settings.textColor });
 
 // Geometries
 const textGeometries = new Map();
-fontLoader.load("/fonts/autour.json", (font) => {
-  textGeometries.set("autour", createTextGeometries(font));
-  if (dices.length === 0) createDices();
-});
+fontLoader.load("/fonts/autour.json", (font) =>
+  textGeometries.set("autour", createTextGeometries(font))
+);
 fontLoader.load("/fonts/kanit.json", (font) =>
   textGeometries.set("kanit", createTextGeometries(font))
 );
@@ -70,6 +73,7 @@ const labelTextGeometries = [...Array(6).keys()].map((i) => i + 1);
 const clock = new THREE.Clock();
 let oldElapsedTime = 0;
 
+// GUI
 const gui = new GUI();
 gui
   .add(settings, "totalDices")
@@ -83,11 +87,29 @@ gui
   .max(3)
   .step(1)
   .onFinishChange((fontKey) => createDices(settings.totalDices, fontKey));
-
+gui
+  .addColor(settings, "color")
+  .onChange(() => material.color.set(settings.color));
+gui
+  .addColor(settings, "textColor")
+  .onChange(() => textMaterial.color.set(settings.textColor));
+gui
+  .add(settings, "metalness")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .onChange(() => (material.metalness = settings.metalness));
+gui
+  .add(settings, "roughness")
+  .min(0)
+  .max(1)
+  .step(0.001)
+  .onChange(() => (material.roughness = settings.roughness));
 gui.add(settings, "throw");
 
 function tick() {
   requestAnimationFrame(tick);
+  if (dices.length === 0) createDices();
   const elapsedTime = clock.getElapsedTime();
   const deltaTime = elapsedTime - oldElapsedTime;
   oldElapsedTime = elapsedTime;
@@ -107,7 +129,7 @@ function createTextGeometries(font) {
     const textGeometry = new TextGeometry(label.toString(), {
       font,
       size: 0.5,
-      height: 0.02,
+      height: 0.01,
     }).center();
     textGeometry.name = label;
     textGeometries.push(textGeometry);
@@ -115,7 +137,7 @@ function createTextGeometries(font) {
   return textGeometries;
 }
 
-function createDices(total = settings.totalDices, key = 1) {
+function createDices(total = settings.totalDices, key = settings.font) {
   const font = fonts.get(key);
   removeDices();
   for (let i = 0; i < total; i++) {
